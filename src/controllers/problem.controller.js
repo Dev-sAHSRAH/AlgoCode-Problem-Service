44fb71d03cd2551e5a1e016239630c9c2387df39
harsha <c.harsha0909@gmail.com>
2024-03-25 18:37:59 +0530

    Feat: Added not implemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,29 +1,41 @@
+const {StatusCodes} = require('http-status-codes')
+
 function pingProblemController(req,res){
     res.json(
         {
-            messge: "ping Service is alive."
+            message: "Ping constroller is up."
         }
     )
 }
 
 function addProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not Implemented'
+    })
 }
 
 function getProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not Implemented'
+    })
 }
 
 function getProblems(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not Implemented'
+    })
 }
 
 function updateProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not Implemented'
+    })
 }
 
 function deleteProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not Implemented'
+    })
 }
 
 module.exports = {
