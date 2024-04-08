8c602a241335bd3e3895023f20613b7028f1df99
harsha <c.harsha0909@gmail.com>
2024-04-08 12:31:26 +0530

    Feat: Added mongoose configuration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/server.config');
 const apiRouter = require('./routes')
 const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
 
 const app = express();
 
@@ -23,7 +24,10 @@ app.get('/ping',(req,res)=>{
 
 app.use(errorHandler);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, async() => {
     console.log(`Server started @ PORT:${PORT} 🚀`);
+
+    await connectToDB();
+    console.log('Successfully connected to DB 🔥');
 })
 
